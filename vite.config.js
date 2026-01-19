51dae36e2b880b78d436666b6dd949c976e8cd8d
JoaoVitor292 <joaovitoalves77@gmail.com>
2026-01-19 11:33:09 -0300

    commit ajuste1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Rotei/',
+  base: '/rotei-app/',
   plugins: [react(), tailwindcss()]
 })
